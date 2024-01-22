d492c40ed3f5d6c52bf0be42797333ff8156e669
Jeremy Panjaitan <jeremy.panjaitan@nanovest.io>
2024-01-22 21:22:58 +0700

    Fixed livewire rendering

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,6 +16,9 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    @livewireStyles
+
 </head>
 <body>
     <div id="app">
@@ -73,8 +77,9 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+            {{ $slot }}
         </main>
     </div>
+    @livewireScripts
 </body>
 </html>
